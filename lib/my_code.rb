89a9024f29014e4a4c5d42cbfdbb0877eb662815
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-14 03:11:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map(source_array)
   new_array = []
   index = 0 
   while index < source_array do 
-    new_array.push(yield(source_array[index]))
+    new_array.push((yield(source_array[index]))
     i += 1 
   end
   new_array
